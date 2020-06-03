2fd5b49a87332d6d1577713410ff7ee63b78bbcc
chakrao <114460470+chakrao@users.noreply.github.com>
2020-06-03 10:39:47 +0000

    Added a post

--- Cheat Engine/betterControls/cecustombutton.pas
+++ Cheat Engine/betterControls/cecustombutton.pas
@@ -101,4 +101,190 @@ type
     property DrawBorder: Boolean read fDrawBorder write setDrawBorder;
     property BorderColor: TColor read fBorderColor write setBorderColor;
     property BorderSize: integer read fBorderSize write setBorderSize;
-    property Button
+    property ButtonColor: TColor read fButtonColor write setButtonColor;
+    property ButtonHighlightedColor: TColor read fButtonHighlightedColor write fButtonHighlightedColor;
+    property ButtonDownColor: TColor read fButtonDownColor write fButtonDownColor;
+
+    property RoundingX: integer read froundingX write setRoundingX;
+    property RoundingY: integer read froundingY write setRoundingY;
+    property CustomDrawn: boolean read fCustomDrawn write setCustomDrawn;
+    property GrowFont: boolean read fGrowFont write setGrowFont;
+    property FramesPerSecond: integer read fFramesPerSecond write setFramesPerSecond;
+    property ButtonAnimationSpeed: dword read fbuttonAnimationSpeed write fbuttonAnimationSpeed;
+    property DrawFocusRect: boolean read fDrawFocusRect write setDrawFocusRect;
+    property FocusedSize: integer read fFocusedSize write setFocusedSize;
+    property FocusElipseColor: tcolor read fFocusElipseColor write fFocusElipseColor;
+    property Scaled: boolean read fScaled write fScaled default true;
+
+    property Align;
+    property Anchors;
+    property AutoSize;
+    property BidiMode;
+    property BorderSpacing;
+    //todo: property Cancel;
+    property Caption: string read getCaption write setCaption;
+    property Color;
+    property Constraints;
+    //todo: property Default;
+    property DoubleBuffered;
+    property DragCursor;
+    property DragKind;
+    property DragMode;
+    property Enabled;
+    property Font;
+    property ParentBidiMode;
+    //todo: property ModalResult;
+    property OnChangeBounds;
+    property OnClick;
+    property OnContextPopup;
+    property OnDragDrop;
+    property OnDragOver;
+    property OnEndDrag;
+    property OnEnter;
+    property OnExit;
+    property OnKeyDown;
+    property OnKeyPress;
+    property OnKeyUp;
+    property OnMouseDown;
+    property OnMouseEnter;
+    property OnMouseLeave;
+    property OnMouseMove;
+    property OnMouseUp;
+    property OnMouseWheel;
+    property OnMouseWheelDown;
+    property OnMouseWheelUp;
+    property OnResize;
+    property OnStartDrag;
+    property OnUTF8KeyPress;
+    property ParentDoubleBuffered;
+    property ParentFont;
+    property ParentShowHint;
+    property PopupMenu;
+    property ShowHint;
+    property TabOrder;
+    property TabStop;
+    property Visible;
+
+
+  {
+    property framelist: TList read fimagelist write fimagelist; //if set it's an animated button instead of a rounded button
+    property currentFrame: integer read fCurrentFrame write fCurrentFrame;
+
+    //0=no automated animation
+    property onLastFrame: TNotifyEvent read fOnLastFrame write fOnLastFrame; //when the last frame of the imagelist has been drawn.  The user can then swap the imagelist, or leave it be , and/or change the currentframe back to 0
+
+    }
+  end;
+
+implementation
+
+uses forms;
+
+procedure TCECustomButton.timertimer(sender: TObject);
+begin
+  Invalidate;
+end;
+
+procedure TCECustomButton.stopAnimatorTimer;
+begin
+  if timer<>nil then timer.enabled:=false;
+end;
+
+procedure TCECustomButton.startAnimatorTimer;
+begin
+  if fFramesPerSecond>0 then
+  begin
+    if timer=nil then
+      timer:=TTimer.create(self);
+
+    timer.OnTimer:=@timertimer;
+    timer.interval:=1000 div fFramesPerSecond;
+    timer.enabled:=true;
+  end
+  else
+    if timer<>nil then freeandnil(timer);
+end;
+
+procedure TCECustomButton.setFramesPerSecond(fps: integer);
+begin
+  fFramesPerSecond:=fps;
+  if (fFramesPerSecond<=0) and (timer<>nil) then
+    freeandnil(timer);
+
+  if timer<>nil then
+    timer.interval:=1000 div fps
+end;
+
+procedure TCECustomButton.setRoundingX(x: integer);
+begin
+  if x>=0 then
+  begin
+    hasSetRounding:=true;
+    froundingX:=x;
+
+    if AutoSize then DoAutoSize;
+    invalidate;
+  end;
+end;
+
+procedure TCECustomButton.setRoundingY(y: integer);
+begin
+  if y>=0 then
+  begin
+    hasSetRounding:=true;
+    froundingY:=y;
+
+    if AutoSize then DoAutoSize;
+    invalidate;
+  end;
+end;
+
+procedure TCECustomButton.setBorderColor(c: tcolor);
+begin
+  fBorderColor:=c;
+  invalidate;
+end;
+
+procedure TCECustomButton.setBorderSize(size: integer);
+begin
+  fBorderSize:=size;
+  invalidate;
+end;
+
+procedure TCECustomButton.setFocusedSize(size: integer);
+begin
+  fFocusedSize:=size;
+  invalidate;
+end;
+
+procedure TCECustomButton.setButtonColor(c: tcolor);
+begin
+  fButtonColor:=c;
+  Invalidate;
+end;
+
+procedure TCECustomButton.setCustomDrawn(state: boolean);
+begin
+  fCustomDrawn:=state;
+  invalidate;
+end;
+
+procedure TCECustomButton.setGrowFont(state: boolean);
+begin
+  fgrowFont:=state;
+  invalidate;
+end;
+
+procedure TCECustomButton.setDrawBorder(state: boolean);
+begin
+  fDrawBorder:=state;
+  invalidate;;
+end;
+
+procedure TCECustomButton.setShowPrefix(state: boolean);
+begin
+  fShowPrefix:=state;
+  invalidate;;
+end;
+
+procedure TCECustom
