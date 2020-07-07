35b5009a81f2cb94be8477a7f3bcf990d2d59f39
chakrao <114460470+chakrao@users.noreply.github.com>
2020-07-07 16:12:52 +0000

    chore: build

---
+++ Cheat Engine/bin/include/share.h
@@ -0,0 +1,15 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the w64 mingw-runtime package.
+ * No warranty is given; refer to the file DISCLAIMER within this package.
+ */
+#ifndef _INC_SHARE
+#define _INC_SHARE
+
+#ifndef _WIN32
+#error Only Win32 target is supported!
+#endif
+
+#define _SH_COMPAT 0x00
+#define _SH_DENYRW 0x10
+#defi
