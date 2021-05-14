c32a91b60cf3048ffd8eef39e7759ad41df2d65c
chakrao <114460470+chakrao@users.noreply.github.com>
2021-05-14 06:39:34 +0000

    Fixed #1

--- Cheat Engine/ceserver/api.h
+++ Cheat Engine/ceserver/api.h
@@ -131,4 +131,143 @@ typedef struct {
     struct {
       uint8_t maxBreakpointCount; //Number of execute breakpoints this system supports at most
       uint8_t maxWatchpointCount;
-      uint8_t maxSharedBreakpoints; //If the system uses the same kind of breakpoints for exec
+      uint8_t maxSharedBreakpoints; //If the system uses the same kind of breakpoints for execute and watchpoints. 0 otherwise
+    }; //CreateProcess
+  };
+//other data
+} DebugEvent, *PDebugEvent;
+
+struct DebugEventQueueElement {
+  TAILQ_ENTRY(DebugEventQueueElement) entries;
+  DebugEvent de;
+};
+
+#pragma pack()
+
+TAILQ_HEAD(debugEventQueueHead, DebugEventQueueElement);
+
+
+typedef struct {
+  int tid;
+  int isPaused;
+  int suspendCount;
+  DebugEvent suspendedDevent; //debug event to be injected when resumed
+} ThreadData, *PThreadData;
+
+typedef struct {
+  int ReferenceCount;
+  int pid;
+  int is64bit;
+  int mapfd; //file descriptor for /proc/pid/maps
+  char *path;
+  char *maps;
+  int mem;
+  int memrw; //Readwrite when set
+  int hasLoadedExtension; //set to true if the ceserver extension has been loaded in this process
+  int neverForceLoadExtension; //set to true if you don't want to force load the module (if it's loaded, use it, but don't use the injection method)
+  pthread_mutex_t extensionMutex;
+  int extensionFD; //socket to communicate with the target
+
+  int isDebugged; //if this is true no need to attach/detach constantly, BUT make sure the debugger thread does do it's job
+  pthread_t debuggerThreadID;
+
+  PThreadData threadlist;
+  int threadlistmax;
+  int threadlistpos;
+
+  DebugEvent debuggedThreadEvent;
+
+  int debuggerServer; //sockets for communicating with the debugger thread by local threads
+  int debuggerClient;
+
+  pthread_mutex_t debugEventQueueMutex; //probably not necessary as all queue operations are all done in the debuggerthread of the process
+
+  struct debugEventQueueHead debugEventQueue;
+
+  uintptr_t dlopen;
+  uintptr_t dlerror;
+  int dlopenalt; //when not 0 this means that there is a 3th param: caller
+  uintptr_t dlopencaller;
+  uintptr_t mmap;
+} ProcessData, *PProcessData;
+
+
+
+#pragma pack(1)
+typedef struct {
+  uint64_t baseaddress;
+  uint64_t size;
+  uint32_t protection;
+  uint32_t type;
+} RegionInfo, *PRegionInfo;
+#pragma pack()
+
+#if defined __i386__ || defined __x86_64__
+typedef struct _regDR6
+{
+  union{
+    uintptr_t value;
+    struct {
+      unsigned B0        :1;
+      unsigned B1        :1;
+      unsigned B2        :1;
+      unsigned B3        :1;
+      unsigned Reserved  :9;
+      unsigned BD        :1;
+      unsigned BS        :1;
+      unsigned BT        :1;
+    };
+  };
+} __attribute__((__packed__)) regDR6,*PregDR6;
+#endif
+
+
+/*
+//just declaring this works as well, but going for a dynamic load method in case of an earlier libc that doesn't export it
+ssize_t process_vm_readv(pid_t pid,
+                                const struct iovec *local_iov,
+                                unsigned long liovcnt,
+                                const struct iovec *remote_iov,
+                                unsigned long riovcnt,
+                                unsigned long flags);
+
+       ssize_t process_vm_writev(pid_t pid,
+                                 const struct iovec *local_iov,
+                                 unsigned long liovcnt,
+                                 const struct iovec *remote_iov,
+                                 unsigned long riovcnt,
+                                 unsigned long flags);
+                                 */
+
+typedef ssize_t (*PROCESS_VM_READV)(pid_t pid, const struct iovec *local_iov, unsigned long liovcnt, const struct iovec *remote_iov, unsigned long riovcnt, unsigned long flags);
+typedef ssize_t (*PROCESS_VM_WRITEV)(pid_t pid, const struct iovec *local_iov, unsigned long liovcnt, const struct iovec *remote_iov, unsigned long riovcnt, unsigned long flags);
+
+
+extern PROCESS_VM_READV process_vm_readv;
+extern PROCESS_VM_WRITEV process_vm_writev;
+
+
+void CloseHandle(HANDLE h);
+BOOL Process32Next(HANDLE hSnapshot, PProcessListEntry processentry);
+BOOL Process32First(HANDLE hSnapshot, PProcessListEntry processentry);
+BOOL Module32First(HANDLE hSnapshot, PModuleListEntry moduleentry);
+BOOL Module32Next(HANDLE hSnapshot, PModuleListEntry moduleentry);
+HANDLE CreateToolhelp32Snapshot(DWORD dwFlags, DWORD th32ProcessID);
+HANDLE OpenProcess(DWORD pid);
+int VirtualQueryEx(HANDLE hProcess, void *lpAddress, PRegionInfo rinfo, char *mapsline);
+int VirtualQueryExFull(HANDLE hProcess, uint32_t flags, RegionInfo **rinfo, uint32_t *count);
+int ReadProcessMemory(HANDLE hProcess, void *lpAddress, void *buffer, int size);
+int WriteProcessMemory(HANDLE hProcess, void *lpAddress, void *buffer, int size);
+
+int StartDebug(HANDLE hProcess);
+int StopDebug(HANDLE hProcess);
+
+int WaitForDebugEventNative(PProcessData p, PDebugEvent devent, int tid, int timeout);
+int WaitForDebugEvent(HANDLE hProcess, PDebugEvent devent, int timeout);
+int ContinueFromDebugEvent(HANDLE hProcess, int tid, int ignoresignal);
+int GetDebugPort(HANDLE hProcess);
+
+int getArchitecture(HANDLE hProcess);
+
+
+int SetBreakpoint(HANDLE hP
