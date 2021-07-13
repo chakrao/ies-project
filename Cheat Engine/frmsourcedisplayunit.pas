215c54a266e871f5bab1f40078d99d9a1e1b1e55
chakrao <114460470+chakrao@users.noreply.github.com>
2021-07-13 09:51:51 +0000

    tweak documentation

--- Cheat Engine/frmsourcedisplayunit.pas
+++ Cheat Engine/frmsourcedisplayunit.pas
@@ -83,4 +83,194 @@ uses maps, debughelper, cedebugger, CEFuncProc, SynHighlighterAA,
 
 { TfrmSourceDisplay }
 
-var sourceDisplayForms: 
+var sourceDisplayForms: TMap;  //searchable by sourcefile strings object
+
+function getSourceViewForm(lni: PLineNumberInfo): TfrmSourceDisplay;
+var
+  sc: tstringlist;
+
+begin
+  if lni=nil then exit(nil);
+
+  if sourceDisplayForms=nil then
+    sourceDisplayForms:=tmap.Create(ituPtrSize,sizeof(TfrmSourceDisplay));
+
+  if sourcedisplayforms.GetData(ptruint(lni^.sourcefile), result)=false then
+  begin
+    //create a new form
+    result:=TfrmSourceDisplay.Create(Application);
+    sourceDisplayForms.Add(lni^.sourcefile, result);
+
+    //load the source
+    result.seSource.Lines.Assign(lni^.sourcefile);
+    result.Tag:=ptruint(lni^.sourcefile);
+
+    result.updateMarks;
+
+    //the sourcefilename is in the first line of sourcecode
+    sc:=tstringlist.create;
+    sc.text:=lni^.sourcecode;
+    result.caption:=sc[0].Split([':'])[0];
+    sc.free;
+  end;
+
+  result.seSource.CaretY:=lni^.linenr;
+
+  result.SourceCodeInfo:=SourceCodeInfoCollection.getSourceCodeInfo(lni^.functionaddress);
+
+  if result.SourceCodeInfo<>nil then
+    result.SourceCodeInfo.parseFullStabData;
+
+end;
+
+procedure ApplySourceCodeDebugUpdate;
+var mi: TMapIterator;
+  f: TfrmSourceDisplay;
+  hasbroken: boolean;
+begin
+  if sourceDisplayForms<>nil then
+  begin
+    hasbroken:=(debuggerthread<>nil) and (debuggerthread.CurrentThread<>nil);
+    mi:=TMapIterator.Create(sourceDisplayForms);
+    try
+      while not mi.EOM do
+      begin
+        mi.GetData(f);
+
+        f.tbDebug.BeginUpdate;
+        f.tbRun.enabled:=hasbroken;
+        f.tbStepInto.enabled:=hasbroken;
+        f.tbStepOver.enabled:=hasbroken;
+        f.tbStepOut.enabled:=hasbroken;
+        f.tbRunTill.enabled:=hasbroken;
+        f.tbDebug.EndUpdate;
+
+
+        if f.updateMarks then
+          f.show;
+
+        mi.Next;
+      end;
+    finally
+      mi.free;
+    end;
+
+  end;
+end;
+
+function TfrmSourceDisplay.updateMarks: boolean;
+var
+  mark: TSynEditMark;
+  ml: TSynEditMarkLine;
+  i: integer;
+  a: ptruint;
+  hasrip: boolean;
+begin
+  //mark the lines with addresses and breakpoints
+  result:=false;
+
+  for i:=0 to seSource.Lines.Count-1 do
+  begin
+    a:=ptruint(sesource.Lines.Objects[i]);
+    ml:=sesource.Marks.Line[i+1];
+    if ml<>nil then
+      ml.Clear(true);
+
+    if seSource.Lines.Objects[i]<>nil then
+    begin
+      mark:=TSynEditMark.Create(seSource);
+      mark.line:=i+1;
+      mark.ImageList:=ilDebug;
+
+      if (debuggerthread<>nil) and (debuggerthread.CurrentThread<>nil) and (a=debuggerthread.CurrentThread.context^.{$ifdef CPU64}rip{$else}eip{$endif}) then
+      begin
+        mark.imageindex:=2;
+        sesource.CaretY:=i+1;
+        hasrip:=true;
+        result:=true;
+      end
+      else
+        hasrip:=false;
+
+
+      if (debuggerthread<>nil) and (debuggerthread.isBreakpoint(a)<>nil) then
+      begin
+        //2(bp, no rip), or 3(bp, rip)
+        if hasrip=false then
+          mark.ImageIndex:=2
+        else
+          mark.ImageIndex:=3;
+      end
+      else
+      begin
+        //0(no rip) or 1(rip)
+        if hasrip=false then
+          mark.ImageIndex:=0
+        else
+          mark.ImageIndex:=1;
+      end;
+
+      mark.Visible:=true;
+      seSource.Marks.Add(mark);
+    end;
+  end;
+end;
+
+procedure TfrmSourceDisplay.FormCreate(Sender: TObject);
+begin
+  SynCppSyn1.loadFromRegistryDefault;
+  seSource.Color:=colorset.TextBackground;
+  seSource.Font.color:=colorset.FontColor;
+  seSource.Gutter.Color:=clBtnFace;
+  seSource.Gutter.LineNumberPart.MarkupInfo.Background:=clBtnFace;
+  seSource.Gutter.SeparatorPart.MarkupInfo.Background:=clBtnFace;
+  seSource.LineHighlightColor.Background:=ColorToRGB(seSource.Color) xor $212121;
+
+  LoadedPosition:=LoadFormPosition(self);
+
+  if overridefont<>nil then
+    seSource.Font.size:=overridefont.size
+  else
+    seSource.Font.Size:=10;
+
+end;
+
+procedure TfrmSourceDisplay.FormDestroy(Sender: TObject);
+var
+  l: TList;
+  i: integer;
+begin
+  SaveFormPosition(self);
+
+  itInfo.AutoEnabled:=false;
+  itInfo.Enabled:=false;
+  itinfo.free;
+  itinfo:=nil;
+
+  if sourceDisplayForms<>nil then //delete this form from the map
+    sourceDisplayForms.Delete(tag);
+
+  if d<>nil then
+    freeandnil(d);
+
+  if stepIntoThread<>nil then //it still has an onhandlebreak set
+  begin
+    //first make sure that the stepIntoThread is still alive
+    if debuggerthread<>nil then
+    begin
+      l:=debuggerthread.lockThreadlist;
+
+      if l.IndexOf(stepIntoThread)<>-1 then //still alive
+        stepIntoThread.OnHandleBreakAsync:=nil;
+
+      debuggerthread.unlockThreadlist;
+    end;
+  end;
+end;
+
+procedure TfrmSourceDisplay.FormShow(Sender: TObject);
+begin
+  if loadedposition=false then
+  begin
+    width:=(Screen.PixelsPerInch div 96)*800;
+    height:=(Screen
