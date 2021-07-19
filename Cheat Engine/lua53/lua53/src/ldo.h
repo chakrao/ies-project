b5daa6204f1fc7881a37726921d65ae86c97c6b8
chakrao <114460470+chakrao@users.noreply.github.com>
2021-07-19 10:55:35 +0000

    fix positioning

---
+++ Cheat Engine/lua53/lua53/src/ldo.h
@@ -0,0 +1,29 @@
+/*
+** $Id: ldo.h,v 2.21 2014/10/25 11:50:46 roberto Exp $
+** Stack and Call structure of Lua
+** See Copyright Notice in lua.h
+*/
+
+#ifndef ldo_h
+#define ldo_h
+
+
+#include "lobject.h"
+#include "lstate.h"
+#include "lzio.h"
+
+
+#define luaD_checkstack(L,n)	if (L->stack_last - L->top <= (n)) \
+				    luaD_growstack(L, n); else condmovestack(L);
+
+
+#define incr_top(L) {L->top++; luaD_checkstack(L,0);}
+
+#define savestack(L,p)		((char *)(p) - (char *)L->stack)
+#define restorestack(L,n)	((TValue *)((char *)L->stack + (n)))
+
+
+/* type of protected functions, to be ran by 'runprotected' */
+typedef void (*Pfunc) (lua_State *L, void *ud);
+
+LUAI_FUNC int luaD_protectedparser (lua_State *L, ZIO *z, const char *name
