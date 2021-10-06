9b2c2d89dc5df454f3fffbbe978c878157963ce0
chakrao <114460470+chakrao@users.noreply.github.com>
2021-10-06 13:37:56 +0000

    more linting

--- Cheat Engine/tcclib/lib/bcheck.c
+++ Cheat Engine/tcclib/lib/bcheck.c
@@ -147,4 +147,138 @@ static pthread_spinlock_t bounds_spin;
 #define TRY_SEM()              if (use_sem) pthread_spin_trylock (&bounds_spin)
 #endif
 #define HAVE_MEMALIGN          (1)
-#define MALLOC_REDIR
+#define MALLOC_REDIR           (1)
+#define HAVE_PTHREAD_CREATE    (1)
+#define HAVE_CTYPE             (1)
+#define HAVE_ERRNO             (1)
+#define HAVE_SIGNAL            (1)
+#define HAVE_SIGACTION         (1)
+#define HAVE_FORK              (1)
+#if !defined(__APPLE__) && defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
+#define HAVE_TLS_FUNC          (0)
+#define HAVE_TLS_VAR           (1)
+#else
+#define HAVE_TLS_FUNC          (1)
+#define HAVE_TLS_VAR           (0)
+#endif
+#ifdef TCC_MUSL
+# undef HAVE_CTYPE
+#endif
+#endif
+
+#if MALLOC_REDIR
+static void *(*malloc_redir) (size_t);
+static void *(*calloc_redir) (size_t, size_t);
+static void (*free_redir) (void *);
+static void *(*realloc_redir) (void *, size_t);
+static unsigned int pool_index;
+static unsigned char __attribute__((aligned(16))) initial_pool[256];
+#endif
+#if HAVE_MEMALIGN
+static void *(*memalign_redir) (size_t, size_t);
+#endif
+#if HAVE_PTHREAD_CREATE
+static int (*pthread_create_redir) (pthread_t *thread,
+                                    const pthread_attr_t *attr,
+                                    void *(*start_routine)(void *), void *arg);
+#endif
+#if HAVE_SIGNAL
+typedef void (*bound_sig)(int);
+static bound_sig (*signal_redir) (int signum, bound_sig handler);
+#endif
+#if HAVE_SIGACTION
+static int (*sigaction_redir) (int signum, const struct sigaction *act,
+                               struct sigaction *oldact);
+#endif
+#if HAVE_FORK
+static int (*fork_redir) (void);
+#endif
+
+#define TCC_TYPE_NONE           (0)
+#define TCC_TYPE_MALLOC         (1)
+#define TCC_TYPE_CALLOC         (2)
+#define TCC_TYPE_REALLOC        (3)
+#define TCC_TYPE_MEMALIGN       (4)
+#define TCC_TYPE_STRDUP         (5)
+
+/* this pointer is generated when bound check is incorrect */
+#define INVALID_POINTER ((void *)(-2))
+
+typedef struct tree_node Tree;
+struct tree_node {
+    Tree * left, * right;
+    size_t start;
+    size_t size;
+    unsigned char type;
+    unsigned char is_invalid; /* true if pointers outside region are invalid */
+};
+
+typedef struct alloca_list_struct {
+    size_t fp;
+    void *p;
+    size_t size;
+    struct alloca_list_struct *next;
+} alloca_list_type;
+
+#if defined(_WIN32)
+#define BOUND_TID_TYPE   DWORD
+#define BOUND_GET_TID    GetCurrentThreadId()
+#elif defined(__OpenBSD__)
+#define BOUND_TID_TYPE   pid_t
+#define BOUND_GET_TID    syscall (SYS_getthrid)
+#elif defined(__FreeBSD__) || defined(__NetBSD__)
+#define BOUND_TID_TYPE   pid_t
+#define BOUND_GET_TID    0
+#elif defined(__i386__) || defined(__x86_64__) || defined(__arm__) || defined(__aarch64__) || defined(__riscv)
+#define BOUND_TID_TYPE   pid_t
+#define BOUND_GET_TID    syscall (SYS_gettid)
+#else
+#define BOUND_TID_TYPE   int
+#define BOUND_GET_TID    0
+#endif
+
+typedef struct jmp_list_struct {
+    void *penv;
+    size_t fp;
+    size_t end_fp;
+    BOUND_TID_TYPE tid;
+    struct jmp_list_struct *next;
+} jmp_list_type;
+
+#define BOUND_STATISTIC_SPLAY   (0)
+static Tree * splay (size_t addr, Tree *t);
+static Tree * splay_end (size_t addr, Tree *t);
+static Tree * splay_insert(size_t addr, size_t size, Tree * t);
+static Tree * splay_delete(size_t addr, Tree *t);
+void splay_printtree(Tree * t, int d);
+
+/* external interface */
+void __bounds_checking (int no_check);
+void __bound_never_fatal (int no_check);
+DLL_EXPORT void * __bound_ptr_add(void *p, size_t offset);
+DLL_EXPORT void * __bound_ptr_indir1(void *p, size_t offset);
+DLL_EXPORT void * __bound_ptr_indir2(void *p, size_t offset);
+DLL_EXPORT void * __bound_ptr_indir4(void *p, size_t offset);
+DLL_EXPORT void * __bound_ptr_indir8(void *p, size_t offset);
+DLL_EXPORT void * __bound_ptr_indir12(void *p, size_t offset);
+DLL_EXPORT void * __bound_ptr_indir16(void *p, size_t offset);
+DLL_EXPORT void FASTCALL __bound_local_new(void *p1);
+DLL_EXPORT void FASTCALL __bound_local_delete(void *p1);
+void __bound_init(size_t *, int);
+void __bound_main_arg(int argc, char **argv, char **envp);
+void __bound_exit(void);
+#if !defined(_WIN32)
+void *__bound_mmap (void *start, size_t size, int prot, int flags, int fd,
+                    off_t offset);
+int __bound_munmap (void *start, size_t size);
+DLL_EXPORT void __bound_siglongjmp(jmp_buf env, int val);
+#endif
+DLL_EXPORT void __bound_new_region(void *p, size_t size);
+DLL_EXPORT void __bound_setjmp(jmp_buf env);
+DLL_EXPORT void __bound_longjmp(jmp_buf env, int val);
+DLL_EXPORT void *__bound_memcpy(void *dst, const void *src, size_t size);
+DLL_EXPORT int __bound_memcmp(const void *s1, const void *s2, size_t size);
+DLL_EXPORT void *__bound_memmove(void *dst, const void *src, size_t size);
+DLL_EXPORT void *__bound_memset(void *dst, int c, size_t size);
+DLL_EXPORT int __bound_strlen(const char *s);
+DLL_EXPORT char *__bound_strcpy(char *dst, const c
