315e3d4ac1562727d9174f07140d570808eb789f
chakrao <114460470+chakrao@users.noreply.github.com>
2021-10-06 11:44:41 +0000

    Rename no to nb (step 4/4): Change 'no' to 'nb' in remaining files.

--- Cheat Engine/tcclib/lib/bcheck.c
+++ Cheat Engine/tcclib/lib/bcheck.c
@@ -11,4 +11,140 @@
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for mor
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <setjmp.h>
+
+#if !defined(__FreeBSD__) \
+ && !defined(__FreeBSD_kernel__) \
+ && !defined(__DragonFly__) \
+ && !defined(__OpenBSD__) \
+ && !defined(__APPLE__) \
+ && !defined(__NetBSD__)
+#include <malloc.h>
+#endif
+
+#if !defined(_WIN32)
+#include <unistd.h>
+#include <sys/syscall.h>
+#endif
+
+#define BOUND_DEBUG             (1)
+#define BOUND_STATISTIC         (1)
+
+#if BOUND_DEBUG
+ #define dprintf(a...)         if (print_calls) fprintf(a)
+#else
+ #define dprintf(a...)
+#endif
+
+#ifdef __attribute__
+  /* an __attribute__ macro is defined in the system headers */
+  #undef __attribute__ 
+#endif
+#define FASTCALL __attribute__((regparm(3)))
+
+#ifdef _WIN32
+# define DLL_EXPORT __declspec(dllexport)
+#else
+# define DLL_EXPORT
+#endif
+
+#if defined(__FreeBSD__) \
+ || defined(__FreeBSD_kernel__) \
+ || defined(__DragonFly__) \
+ || defined(__OpenBSD__) \
+ || defined(__NetBSD__) \
+ || defined(__dietlibc__)
+
+#include <sys/mman.h>
+#define INIT_SEM()
+#define EXIT_SEM()
+#define WAIT_SEM()
+#define POST_SEM()
+#define TRY_SEM()
+#define HAVE_MEMALIGN          (0)
+#define MALLOC_REDIR           (0)
+#define HAVE_PTHREAD_CREATE    (0)
+#define HAVE_CTYPE             (0)
+#define HAVE_ERRNO             (0)
+#define HAVE_SIGNAL            (0)
+#define HAVE_SIGACTION         (0)
+#define HAVE_FORK              (0)
+#define HAVE_TLS_FUNC          (0)
+#define HAVE_TLS_VAR           (0)
+
+#elif defined(_WIN32)
+
+#include <windows.h>
+#include <signal.h>
+static CRITICAL_SECTION bounds_sem;
+#define INIT_SEM()             InitializeCriticalSection(&bounds_sem)
+#define EXIT_SEM()             DeleteCriticalSection(&bounds_sem)
+#define WAIT_SEM()             EnterCriticalSection(&bounds_sem)
+#define POST_SEM()             LeaveCriticalSection(&bounds_sem)
+#define TRY_SEM()              TryEnterCriticalSection(&bounds_sem)
+#define HAVE_MEMALIGN          (0)
+#define MALLOC_REDIR           (0)
+#define HAVE_PTHREAD_CREATE    (0)
+#define HAVE_CTYPE             (0)
+#define HAVE_ERRNO             (0)
+#define HAVE_SIGNAL            (1)
+#define HAVE_SIGACTION         (0)
+#define HAVE_FORK              (0)
+#define HAVE_TLS_FUNC          (1)
+#define HAVE_TLS_VAR           (0)
+
+#else
+
+#define __USE_GNU              /* get RTLD_NEXT */
+#include <sys/mman.h>
+#include <ctype.h>
+#include <pthread.h>
+#include <dlfcn.h>
+#include <errno.h>
+#include <signal.h>
+#ifdef __APPLE__
+#include <dispatch/dispatch.h>
+static dispatch_semaphore_t bounds_sem;
+#define INIT_SEM()             bounds_sem = dispatch_semaphore_create(1)
+#define EXIT_SEM()             dispatch_release(*(dispatch_object_t*)&bounds_sem)
+#define WAIT_SEM()             if (use_sem) dispatch_semaphore_wait(bounds_sem, DISPATCH_TIME_FOREVER)
+#define POST_SEM()             if (use_sem) dispatch_semaphore_signal(bounds_sem)
+#define TRY_SEM()              if (use_sem) dispatch_semaphore_wait(bounds_sem, DISPATCH_TIME_NOW)
+#elif 0
+#include <semaphore.h>
+static sem_t bounds_sem;
+#define INIT_SEM()             sem_init (&bounds_sem, 0, 1)
+#define EXIT_SEM()             sem_destroy (&bounds_sem)
+#define WAIT_SEM()             if (use_sem) while (sem_wait (&bounds_sem) < 0 \
+                                                   && errno == EINTR)
+#define POST_SEM()             if (use_sem) sem_post (&bounds_sem)
+#define TRY_SEM()              if (use_sem) while (sem_trywait (&bounds_sem) < 0 \
+                                                   && errno == EINTR)
+#elif 0
+static pthread_mutex_t bounds_mtx;
+#define INIT_SEM()             pthread_mutex_init (&bounds_mtx, NULL)
+#define EXIT_SEM()             pthread_mutex_destroy (&bounds_mtx)
+#define WAIT_SEM()             if (use_sem) pthread_mutex_lock (&bounds_mtx)
+#define POST_SEM()             if (use_sem) pthread_mutex_unlock (&bounds_mtx)
+#define TRY_SEM()              if (use_sem) pthread_mutex_trylock (&bounds_mtx)
+#else
+static pthread_spinlock_t bounds_spin;
+/* about 25% faster then semaphore. */
+#define INIT_SEM()             pthread_spin_init (&bounds_spin, 0)
+#define EXIT_SEM()             pthread_spin_destroy (&bounds_spin)
+#define WAIT_SEM()             if (use_sem) pthread_spin_lock (&bounds_spin)
+#define POST_SEM()             if (use_sem) pthread_spin_unlock (&bounds_spin)
+#define TRY_SEM()              if (use_sem) pthread_spin_trylock (&bounds_spin)
+#endif
+#define HAVE_MEMALIGN          (1)
+#define MALLOC_REDIR
