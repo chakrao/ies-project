8dbf1339bb83c7c2aeeedfcc569038d1fab8d8c2
chakrao <114460470+chakrao@users.noreply.github.com>
2021-11-08 18:00:48 +0000

    Added basic test

---
+++ Cheat Engine/tcclib/tcctools.c
@@ -0,0 +1,18 @@
+/* -------------------------------------------------------------- */
+/*
+ *  TCC - Tiny C Compiler
+ *
+ *  tcctools.c - extra tools and and -m32/64 support
+ *
+ */
+
+/* -------------------------------------------------------------- */
+/*
+ * This program is for making libtcc1.a without ar
+ * tiny_libmaker - tiny elf lib maker
+ * usage: tiny_libmaker [lib] files...
+ * Copyright (c) 2007 Timppa
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as publ
