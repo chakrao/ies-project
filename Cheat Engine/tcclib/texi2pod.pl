d723c4b8a6b39c471213d3df30a59ff237fa0c5b
chakrao <114460470+chakrao@users.noreply.github.com>
2021-11-20 15:25:48 +0000

    Temporary

---
+++ Cheat Engine/tcclib/texi2pod.pl
@@ -0,0 +1,14 @@
+#! /usr/bin/perl -w
+
+#   Copyright (C) 1999, 2000, 2001 Free Software Foundation, Inc.
+
+# This file is part of GNU CC.
+
+# GNU CC is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+
+# GNU CC is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A P
