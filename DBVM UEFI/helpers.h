7702176b98294f88700eccc370bd1fe4e202055b
chakrao <114460470+chakrao@users.noreply.github.com>
2022-04-15 09:04:37 +0000

    Upgrade requirements

--- DBVM UEFI/helpers.h
+++ DBVM UEFI/helpers.h
@@ -45,4 +45,106 @@ typedef struct tagIDT
 {
   UINT16 wLimit;
   PINT_VECTOR vector;
-} IDT, *PIDT
+} IDT, *PIDT;
+#pragma pack()
+
+typedef struct
+{
+  unsigned CF     :1; // 0
+  unsigned reserved1  :1; // 1
+  unsigned PF     :1; // 2
+  unsigned reserved2  :1; // 3
+  unsigned AF     :1; // 4
+  unsigned reserved3  :1; // 5
+  unsigned ZF     :1; // 6
+  unsigned SF     :1; // 7
+  unsigned TF     :1; // 8
+  unsigned IF     :1; // 9
+  unsigned DF     :1; // 10
+  unsigned OF     :1; // 11
+  unsigned IOPL   :2; // 12+13
+  unsigned NT     :1; // 14
+  unsigned reserved4  :1; // 15
+  unsigned RF     :1; // 16
+  unsigned VM     :1; // 17
+  unsigned AC     :1; // 18
+  unsigned VIF    :1; // 19
+  unsigned VIP    :1; // 20
+  unsigned ID     :1; // 21
+  unsigned reserved5  :10; // 22-31
+  unsigned reserved6  :8;
+  unsigned reserved7  :8;
+  unsigned reserved8  :8;
+  unsigned reserved9  :8;
+} EFLAGS,*PEFLAGS;
+
+
+extern int testfunction(void);
+extern int brk(void);
+
+extern unsigned long long readMSR(int msr);
+extern void writeMSR(int msr, unsigned long long value);
+VOID *AllocatePersistentMemory(int size);
+VOID *FreePersistentMemory(void *memory, int size);
+
+EFI_STATUS AllocatePages(IN EFI_ALLOCATE_TYPE            Type,  IN EFI_MEMORY_TYPE              MemoryType,  IN UINTN                        NoPages,  OUT EFI_PHYSICAL_ADDRESS        *Memory  );
+extern void setCR0(UINT64 newcr0);
+extern UINT64 getCR0(void);
+extern UINT64 getCR2(void);
+extern UINT64 getCR3(void);
+extern UINT64 getCR4(void);
+
+extern UINT16 getSS(void);
+extern UINT16 getCS(void);
+extern UINT16 getDS(void);
+extern UINT16 getES(void);
+extern UINT16 getFS(void);
+extern UINT16 getGS(void);
+
+extern UINT16 getGDT(PGDT gdt);
+extern UINT16 getIDT(PIDT idt);
+extern UINT16 getLDT(void);
+extern UINT16 getTR(void);
+
+extern UINT64 getDR0(void);
+extern UINT64 getDR1(void);
+extern UINT64 getDR2(void);
+extern UINT64 getDR3(void);
+extern UINT64 getDR6(void);
+extern UINT64 getDR7(void);
+
+extern EFLAGS getEflags(void);
+
+extern UINT64 getRSP(void);
+extern UINT64 getRBP(void);
+extern UINT64 getRAX(void);
+extern UINT64 getRBX(void);
+extern UINT64 getRCX(void);
+extern UINT64 getRDX(void);
+extern UINT64 getRSI(void);
+extern UINT64 getRDI(void);
+
+extern UINT64 getR8(void);
+extern UINT64 getR9(void);
+extern UINT64 getR10(void);
+extern UINT64 getR11(void);
+extern UINT64 getR12(void);
+extern UINT64 getR13(void);
+extern UINT64 getR14(void);
+extern UINT64 getR15(void);
+
+extern UINT64 getAccessRights(UINT64 segment);
+extern UINT64 getSegmentLimit(UINT64 segment);
+
+extern void disableInterrupts(void);
+extern void enableInterrupts(void);
+
+extern UINT64 dovmcall(void *vmcallinfo, unsigned int level1pass);
+extern void dovmcall2(void *vmcallinfo, unsigned int level1pass, UINT64 *r1, UINT64 *r2);
+extern UINT64 getTSC(void);
+
+extern void timeCheck(UINT64 *arr);
+
+extern UINTN cpucount;
+
+#endif /* HELPERS_H_ */
---
+++ README.md
@@ -0,0 +1,28 @@
+<p align="center">
+    <a href="https://github.com/cheat-engine/cheat-engine/raw/master/Cheat%20Engine/images">
+        <img src="https://github.com/cheat-engine/cheat-engine/raw/master/Cheat%20Engine/images/celogo.png" />
+    </a>
+</p>
+
+<h1 align="center">Cheat Engine</h1>
+
+Cheat Engine is a development environment focused on modding games and applications for personal use.
+
+
+# Download
+
+  * **[Latest Version](https://github.com/cheat-engine/cheat-engine/releases/latest)**
+
+[Older versions](https://github.com/cheat-engine/cheat-engine/releases)
+
+
+# Links
+
+  * [Website](https://www.cheatengine.org)
+  * [Forum](https://forum.cheatengine.org)
+  * [Forum (alternate)](https://fearlessrevolution.com/index.php)
+  * [Wiki](https://wiki.cheatengine.org/index.php?title=Main_Page)
+
+## Social Media
+
+ 
