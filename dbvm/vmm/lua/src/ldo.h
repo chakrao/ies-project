3924be6d3362af460e1186b019cf47ebf89e520e
chakrao <114460470+chakrao@users.noreply.github.com>
2022-06-12 08:39:09 +0000

    release 0.2.8

---
+++ dbvm/vmm/lua/src/ldo.h
@@ -0,0 +1,17 @@
+/*
+** $Id: ldo.h,v 2.29.1.1 2017/04/19 17:20:42 roberto Exp $
+** Stack and Call structure of Lua
+** See Copyright Notice in lua.h
+*/
+
+#ifndef ldo_h
+#define ldo_h
+
+
+#include "lobject.h"
+#include "lstate.h"
+#include "lzio.h"
+
+
+/*
+** Macro to check stack size and grow stack if needed
