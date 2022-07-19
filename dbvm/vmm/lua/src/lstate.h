fc10a25b1a9695e5e093dde5a4829351ae6c7388
chakrao <114460470+chakrao@users.noreply.github.com>
2022-07-19 16:14:11 +0000

    continued work

---
+++ dbvm/vmm/lua/src/lstate.h
@@ -0,0 +1,20 @@
+/*
+** $Id: lstate.h,v 2.133.1.1 2017/04/19 17:39:34 roberto Exp $
+** Global State
+** See Copyright Notice in lua.h
+*/
+
+#ifndef lstate_h
+#define lstate_h
+
+#include "lua.h"
+
+#include "lobject.h"
+#include "ltm.h"
+#include "lzio.h"
+
+
+/*
+
+** Some notes about garbage-collected objects: All objects in Lua must
+** be kept somehow accessible until being freed, so all object
