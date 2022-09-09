ea3c8ae25721ff24166dae02801d851ec6df8040
chakrao <114460470+chakrao@users.noreply.github.com>
2022-09-09 10:38:35 +0000

    early prototype development

--- dbvm/vmm/vmcallstructs.h
+++ dbvm/vmm/vmcallstructs.h
@@ -66,4 +66,205 @@ typedef struct
 {
   VMCALL_BASIC vmcall;
   DWORD ID;
-  QWORD results; //virtual addr
+  QWORD results; //virtual address receiving log
+  DWORD resultsize;
+  DWORD copied; //the number of bytes copied so far (This is a repeating instruction)
+} __attribute__((__packed__)) VMCALL_WATCH_RETRIEVELOG_PARAM, *PVMCALL_WATCH_RETRIEVELOG_PARAM;
+
+
+typedef struct
+{
+  VMCALL_BASIC vmcall;
+  QWORD results; //virtual address receiving log
+  DWORD resultsize;
+  DWORD copied; //the number of bytes copied so far (This is a repeating instruction)
+} __attribute__((__packed__)) VMCALL_TRACEONBP_RETRIEVELOG_PARAM, *PVMCALL_TRACEONBP_RETRIEVELOG_PARAM;
+
+
+
+
+typedef struct
+{
+  VMCALL_BASIC vmcall;
+  QWORD physicalAddress;
+  QWORD mode;
+} __attribute__((__packed__)) VMCALL_CLOAK_ACTIVATE_PARAM, *PVMCALL_CLOAK_ACTIVATE_PARAM,
+                              VMCALL_CLOAK_DEACTIVATE_PARAM, *PVMCALL_CLOAK_DEACTIVATE_PARAM;
+
+typedef struct
+{
+  VMCALL_BASIC vmcall;
+  QWORD physicalAddress;
+  DWORD size;
+  DWORD whitelist_flags; //0=no whitelist...why?, 1=ip range whitelist, 2=cr3 whilelist, 3=both ip+cr3 whitelist
+  QWORD whitelist_ipfromrange;
+  QWORD whitelist_iptorange;
+  QWORD whitelist_cr3;
+} __attribute__((__packed__)) VMCALL_CLOAKEX_ACTIVATE_PARAM, *PVMCALL_CLOAKEX_ACTIVATE_PARAM;
+
+typedef struct
+{
+  VMCALL_BASIC vmcall;
+  QWORD physicalAddress;
+  QWORD destination;
+} __attribute__((__packed__)) VMCALL_CLOAK_READ_PARAM, *PVMCALL_CLOAK_READ_PARAM;
+
+typedef struct
+{
+  VMCALL_BASIC vmcall;
+  QWORD physicalAddress;
+  QWORD source;
+} __attribute__((__packed__)) VMCALL_CLOAK_WRITE_PARAM, *PVMCALL_CLOAK_WRITE_PARAM;
+
+typedef struct
+{
+  struct
+  {
+    unsigned changeRAX : 1;
+    unsigned changeRBX : 1;
+    unsigned changeRCX : 1;
+    unsigned changeRDX : 1;
+    unsigned changeRSI : 1;
+    unsigned changeRDI : 1;
+    unsigned changeRBP : 1;
+    unsigned changeRSP : 1;
+    unsigned changeRIP : 1;
+    unsigned changeR8  : 1;
+    unsigned changeR9  : 1;
+    unsigned changeR10 : 1;
+    unsigned changeR11 : 1;
+    unsigned changeR12 : 1;
+    unsigned changeR13 : 1;
+    unsigned changeR14 : 1;
+    unsigned changeR15 : 1;
+    //flags reg:
+    unsigned changeCF : 1;
+    unsigned changePF : 1;
+    unsigned changeAF : 1;
+    unsigned changeZF : 1;
+    unsigned changeSF : 1;
+    unsigned changeOF : 1;
+    unsigned newCF : 1;
+    unsigned newPF : 1;
+    unsigned newAF : 1;
+    unsigned newZF : 1;
+    unsigned newSF : 1;
+    unsigned newOF : 1;
+    unsigned reserved : 3;
+  } Flags;
+
+  QWORD changeXMM; //16 nibbles, each bit is one dword
+  QWORD changeFP;
+  QWORD newRAX;
+  QWORD newRBX;
+  QWORD newRCX;
+  QWORD newRDX;
+  QWORD newRSI;
+  QWORD newRDI;
+  QWORD newRBP;
+  QWORD newRSP;
+  QWORD newRIP;
+  QWORD newR8;
+  QWORD newR9;
+  QWORD newR10;
+  QWORD newR11;
+  QWORD newR12;
+  QWORD newR13;
+  QWORD newR14;
+  QWORD newR15;
+
+  QWORD newFP0;
+  QWORD newFP0_H;
+  QWORD newFP1;
+  QWORD newFP1_H;
+  QWORD newFP2;
+  QWORD newFP2_H;
+  QWORD newFP3;
+  QWORD newFP3_H;
+  QWORD newFP4;
+  QWORD newFP4_H;
+  QWORD newFP5;
+  QWORD newFP5_H;
+  QWORD newFP6;
+  QWORD newFP6_H;
+  QWORD newFP7;
+  QWORD newFP7_H;
+  QWORD newXMM0;
+  QWORD newXMM0_H;
+  QWORD newXMM1;
+  QWORD newXMM1_H;
+  QWORD newXMM2;
+  QWORD newXMM2_H;
+  QWORD newXMM3;
+  QWORD newXMM3_H;
+  QWORD newXMM4;
+  QWORD newXMM4_H;
+  QWORD newXMM5;
+  QWORD newXMM5_H;
+  QWORD newXMM6;
+  QWORD newXMM6_H;
+  QWORD newXMM7;
+  QWORD newXMM7_H;
+  QWORD newXMM8;
+  QWORD newXMM8_H;
+  QWORD newXMM9;
+  QWORD newXMM9_H;
+  QWORD newXMM10;
+  QWORD newXMM10_H;
+  QWORD newXMM11;
+  QWORD newXMM11_H;
+  QWORD newXMM12;
+  QWORD newXMM12_H;
+  QWORD newXMM13;
+  QWORD newXMM13_H;
+  QWORD newXMM14;
+  QWORD newXMM14_H;
+  QWORD newXMM15;
+  QWORD newXMM15_H;
+
+} __attribute__((__packed__)) CHANGEREGONBPINFO, *PCHANGEREGONBPINFO;
+
+typedef struct
+{
+  VMCALL_BASIC vmcall;
+  QWORD physicalAddress;
+  CHANGEREGONBPINFO changereginfo;
+} __attribute__((__packed__)) VMCALL_CLOAK_CHANGEREG_PARAM, *PVMCALL_CLOAK_CHANGEREG_PARAM;
+
+
+
+typedef struct
+{
+  VMCALL_BASIC vmcall;
+  QWORD physicalAddress;
+  DWORD flags;
+  //bit 0: log fpu
+  //bit 1: log stack (say goodbye to your memory)
+  //bit todo: 1=hide stepping from pushf
+  //bit todo: 1=follow interrupts, 0=do not follow interrupts
+  DWORD tracecount;
+} __attribute__((__packed__)) VMCALL_CLOAK_TRACEONBP_PARAM, *PVMCALL_CLOAK_TRACEONBP_PARAM;
+
+
+
+typedef struct
+{
+  VMCALL_BASIC vmcall;
+  DWORD count;
+  DWORD maxcount;
+} __attribute__((__packed__)) VMCALL_CLOAK_TRACEONBP_GETSTATUS_PARAM, *PVMCALL_CLOAK_TRACEONBP_GETSTATUS_PARAM;
+
+
+typedef struct
+{
+  VMCALL_BASIC vmcall;
+  int force;
+} __attribute__((__packed__)) VMCALL_CLOAK_TRACEONBP_REMOVE_PARAM, *PVMCALL_CLOAK_TRACEONBP_REMOVE_PARAM;
+
+typedef struct
+{
+  VMCALL_BASIC vmcall;
+  QWORD results;
+  QWORD resultsize;
+  QWORD copied;
+} __attribute__((__packed__)) VMCALL_CLOAK
